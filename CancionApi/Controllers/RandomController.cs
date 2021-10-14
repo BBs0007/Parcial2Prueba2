8c8da3b4278dc1035178517eb2e3405bd0e3e483
BBs0007 <bbsbruno.bbs@gmail.com>
2021-10-13 20:16:10 -0400

    creado el random controller

---
+++ CancionApi/Controllers/RandomController.cs
@@ -0,0 +1,50 @@
+﻿using CancionApi.Data;
+using CancionApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancionApi.Controllers
+{
+    // [Route("api/[controller]")]
+    // [ApiController]
+    // public class RandomController : ControllerBase
+    // {
+    // }
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public RandomController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Cancion>> GetCancion()
+        {
+
+            var list = await _context.Cancion.ToListAsync();
+
+            var max = list.Count;
+            int index = new Random().Next(0, max);
+
+            var cancion = list[index];
+
+            if (cancion == null)
+            {
+                return NoContent();
+            }
+
+            return cancion;
+        }
+    }
+}
